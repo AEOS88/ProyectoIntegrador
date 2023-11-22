fae7a73f68eb87f8b6b5e599b0968287ecd26340
AEOS88 <142273297+AEOS88@users.noreply.github.com>
2023-11-22 15:32:45 -0600

    Implementando una funcion que reciba el mapa del laberinto como cadena.

--- Proyecto Integrador 4.py
+++ Proyecto Integrador 4.py
@@ -1,63 +1,76 @@
-import readchar
 import os
+import readchar
+from typing import List, Tuple
 
 WALL = "#"
 PATH = "."
 PLAYER = "P"
 
-px = 0
-py = 0
+
+def crear_laberinto_desde_cadena(maze_string: str, start: Tuple[int, int], end: Tuple[int, int]) -> List[List[str]]:
+    maze = [list(row) for row in maze_string.strip().split("\n")]
+
+    # Set player start and end positions
+    maze[start[1]][start[0]] = PLAYER
+    maze[end[1]][end[0]] = PATH
+
+    return maze
 
 
-def print_instructions():
-    print("\nINSTRUCCIONES: ")
-    print("\tUsa las flechas para moverte, q para salir")
-
-
-def print_maze(maze):
+def print_maze(maze: List[List[str]]) -> None:
+    os.system('cls' if os.name == 'nt' else 'clear')  # Limpiar pantalla
     for row in maze:
         print("".join(row))
 
-    print_instructions()
 
+def main_loop(maze: List[List[str]], start: Tuple[int, int], end: Tuple[int, int]) -> None:
+    px, py = start
 
-def place_player(maze, x, y, dx, dy):
-    global px, py
-    maze_width = len(maze[0])
-    maze_height = len(maze)
-
-    if x + dx < 0 or x + dx >= maze_width or y + dy < 0 or y + dy >= maze_height:
-        return
-
-    if y + dy == maze_height - 1 and x + dx == maze_width - 1:
-        print("\n\n\t\tG A N A S T E\n\n")
-        exit()
-
-    if maze[y + dy][x + dx] == PATH:
-        maze[y][x] = PATH
-        maze[y + dy][x + dx] = PLAYER
-        px = x + dx
-        py = y + dy
-
-
-with open("maze.txt", "r") as f:
-    maze = list(map(lambda x: list(x), f.read().splitlines()))
-    place_player(maze, px, py, 0, 0)
-
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
+    while (px, py) != end:
         print_maze(maze)
         key = readchar.readkey()
 
         if key == "q":
             break
         elif key.lower() == "a" or key == readchar.key.LEFT:
-            place_player(maze, px, py, -1, 0)
+            if px - 1 >= 0 and maze[py][px - 1] != WALL:
+                maze[py][px] = PATH
+                px -= 1
+                maze[py][px] = PLAYER
         elif key.lower() == "s" or key == readchar.key.DOWN:
-            place_player(maze, px, py, 0, 1)
+            if py + 1 < len(maze) and maze[py + 1][px] != WALL:
+                maze[py][px] = PATH
+                py += 1
+                maze[py][px] = PLAYER
         elif key.lower() == "d" or key == readchar.key.RIGHT:
-            place_player(maze, px, py, 1, 0)
+            if px + 1 < len(maze[0]) and maze[py][px + 1] != WALL:
+                maze[py][px] = PATH
+                px += 1
+                maze[py][px] = PLAYER
         elif key.lower() == "w" or key == readchar.key.UP:
-            place_player(maze, px, py, 0, -1)
+            if py - 1 >= 0 and maze[py - 1][px] != WALL:
+                maze[py][px] = PATH
+                py -= 1
+                maze[py][px] = PLAYER
 
-# Proyecto integrador 4
+
+if __name__ == "__main__":
+    # Configuración del laberinto generado
+    maze_string = """
+    ####################
+    #..................#
+    #.################.#
+    #.#..............#.# 
+    #.#.############.#.#
+    #.#..............#.# 
+    #.################.#
+    #..................#
+    ####################
+    """
+
+    start_position = (0, 0)
+    end_position = (17, 8)
+
+    maze = crear_laberinto_desde_cadena(maze_string, start_position, end_position)
+    main_loop(maze, start_position, end_position)
+
